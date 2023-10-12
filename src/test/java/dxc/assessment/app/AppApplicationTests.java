61ae65bca0372c366e9e960d1ba6c025ad8c37a1
Jason <jason.tay.nw@gmail.com>
2023-10-12 11:01:57 +0800

    First commit
    Initializr base

---
+++ src/test/java/dxc/assessment/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package dxc.assessment.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
